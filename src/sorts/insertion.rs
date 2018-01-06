c1054cfb49644eee42f8d85e2396e3eb77abe3f6
Jacob Williams <jacobaw@gmail.com>
2018-01-05 19:44:43 -0600

    consolidate sort test code

--- src/sorts/insertion.rs
+++ src/sorts/insertion.rs
@@ -16,46 +16,26 @@ mod tests {
     extern crate test;
 
     use super::*;
-    use self::test::{Bencher, black_box};
+    use self::test::Bencher;
     use super::super::test_util::*;
 
     #[test]
-    fn it_handles_empty_array() {
-        let expected: Vec<i32> = vec![];
-        let mut vec = expected.clone();
-        insertion_sort(&mut vec);
-        assert_eq!(expected, vec);
+    fn it_sorts_correctly() {
+        test_basic_properties(&insertion_sort);
     }
 
     #[bench]
-    fn bench_already_sorted(b: &mut Bencher) {
-        let mut vec = (0..5000).collect();
-        b.iter(|| {
-            insertion_sort(&mut vec);
-            black_box(&vec);
-        });
-        assert_sorted(&vec);
+    fn perf_sorted(b: &mut Bencher) {
+        bench_already_sorted(b, &insertion_sort);
     }
 
     #[bench]
-    fn bench_reverse_sorted(b: &mut Bencher) {
-        let reverse_vec: Vec<i64> = (0..5000).rev().collect();
-        b.iter(|| {
-            let mut vec = reverse_vec.clone();
-            insertion_sort(&mut vec);
-            black_box(&vec);
-            assert_sorted(&vec);
-        });
+    fn perf_reverse_sorted(b: &mut Bencher) {
+        bench_reverse_sorted(b, &insertion_sort);
     }
 
     #[bench]
-    fn bench_sort_random(b: &mut Bencher) {
-        let rand_vec = rand_vec();
-        b.iter(|| {
-            let mut vec = rand_vec.clone();
-            insertion_sort(&mut vec);
-            black_box(&vec);
-            assert_sorted(&vec);
-        });
+    fn perf_random(b: &mut Bencher) {
+        bench_sort_random(b, &insertion_sort);
     }
 }
--- src/sorts/mod.rs
+++ src/sorts/mod.rs
@@ -4,12 +4,14 @@ pub mod insertion;
 #[cfg(test)]
 mod test_util {
     extern crate rand;
+    extern crate test;
 
     use std::fmt::Display;
+    use self::test::{Bencher, black_box};
 
-    pub fn rand_vec() -> Vec<i64> {
-        let mut rand_vec = Vec::with_capacity(5000);
-        for _ in 0..5000 {
+    pub fn rand_vec(size: usize) -> Vec<i64> {
+        let mut rand_vec = Vec::with_capacity(size);
+        for _ in 0..size {
             rand_vec.push(rand::random::<i64>());
         }
         return rand_vec;
@@ -19,5 +21,71 @@ mod test_util {
         for i in 1..(vec.len()) {
             assert!(vec[i - 1] <= vec[i], "not sorted, {} not less than {}", vec[i - 1], vec[i]); // TODO provide useful output
         }
+    }
+
+    pub fn test_basic_properties<F>(sort: &F)
+        where F: 'static + Fn(&mut Vec<i64>) {
+
+        {
+            let mut vec: Vec<i64> = vec![];
+            sort(&mut vec);
+            assert!(vec.is_empty());
+        }
+
+        {
+            let mut vec = vec![1i64];
+            sort(&mut vec);
+            assert_sorted(&vec);
+        }
+
+        {
+            let mut vec = (0i64..1000).collect();
+            sort(&mut vec);
+            assert_sorted(&vec);
+        }
+
+        {
+            let mut vec = (0i64..1000).rev().collect();
+            sort(&mut vec);
+            assert_sorted(&vec);
+        }
+
+        {
+            let mut vec = rand_vec(1000);
+            sort(&mut vec);
+            assert_sorted(&vec);
+        }
+    }
+
+    pub fn bench_already_sorted<F>(b: &mut Bencher, sort: &F)
+        where F: 'static + Fn(&mut Vec<i64>) {
+
+        b.iter(|| {
+            let mut vec = (0i64..5000).collect();
+            sort(&mut vec);
+            black_box(&vec);
+        });
+    }
+
+    pub fn bench_reverse_sorted<F>(b: &mut Bencher, sort: &F)
+        where F: 'static + Fn(&mut Vec<i64>) {
+
+        b.iter(|| {
+            let mut vec = (0i64..5000).rev().collect();
+            sort(&mut vec);
+            black_box(&vec);
+        });
+    }
+
+    pub fn bench_sort_random<F>(b: &mut Bencher, sort: &F)
+        where F: 'static + Fn(&mut Vec<i64>) {
+
+        let original = rand_vec(5000);
+
+        b.iter(|| {
+            let mut vec = original.clone();
+            sort(&mut vec);
+            black_box(&vec);
+        });
     }
 }
